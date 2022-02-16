82141a1f01ef695594066d72747a97d0e28c787a
sajjad heydari <sajjadheydari1401@gmail.com>
2022-02-17 01:18:56 +0330

    pinata configs added

--- src/Minter.js
+++ src/Minter.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { connectWallet, getCurrentWalletConnected } from "./interact";
+import { connectWallet, getCurrentWalletConnected } from "./utils/interact";
 
 const Minter = (props) => {
 
@@ -13,7 +13,9 @@ const Minter = (props) => {
   useEffect(async () => {
     const {address, status} = await getCurrentWalletConnected();
     setWallet(address)
-    setStatus(status); 
+    setStatus(status);
+
+    addWalletListener(); 
 }, []);
 
   const connectWalletPressed = async () => {
@@ -25,6 +27,31 @@ const Minter = (props) => {
   const onMintPressed = async () => { //TODO: implement
     
   };
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
 
   return (
     <div className="Minter">
